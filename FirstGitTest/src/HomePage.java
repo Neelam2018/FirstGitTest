eb64181ceb284b2833aec2b3a1e4ec35d486dafd
bawas <bawas@LAPTOP-728ISD49>
2018-12-27 18:24:35 -0800

    My first Git Push

---
+++ FirstGitTest/src/HomePage.java
@@ -0,0 +1,9 @@
+
+public class HomePage {
+	
+public void HomeTest() {
+		
+		System.out.println(" This is Home Test");
+	}
+
+}
---
+++ FirstGitTest/src/LoginPage.java
@@ -0,0 +1,11 @@
+
+public class LoginPage {
+	
+	public void LoginTest() {
+		
+		System.out.println(" This is Login Test");
+	}
+	
+	
+
+}
